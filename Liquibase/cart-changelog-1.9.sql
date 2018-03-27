bdcb92eb27199aa564c14b0d9885441d708041a9
Sruthi <sruthi.av@capgemini.com>
2018-03-27 08:38:33 +0530

    Update cart-changelog-1.9.sql

--- Liquibase/cart-changelog-1.9.sql
+++ Liquibase/cart-changelog-1.9.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
---changeset DevOpsTeam:13
-insert into users (id, username) values (101,'test10');
-insert into users (id, username) values (102,'test11');
+--changeset DevOpsTeam:14
+insert into users (id, username) values (103,'test13');
+insert into users (id, username) values (104,'test14');
 
 
