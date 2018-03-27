500538fc144cda43fbf3c22ec82b89795a3f268e
Sruthi <sruthi.av@capgemini.com>
2018-03-27 10:32:49 +0000

    Update LaunchBrowser.java

--- TestCases/SeleniumTests/src/setup/LaunchBrowser.java
+++ TestCases/SeleniumTests/src/setup/LaunchBrowser.java
@@ -2,7 +2,7 @@ package setup;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
